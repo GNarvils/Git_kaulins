8c59919f00e2d19c19d5292da21d3a9225e4665c
Narvils <gustavs.gn@gmail.com>
2022-01-18 10:44:40 +0200

    Papildinats ar ciklu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,16 +1,17 @@
 import java.util.Random;
 
 public class Kaulins {
-    static void mestkaulinu(){
+    static void mestkaulinu(int a){
 		int skaitlis;
 		Random rand = new Random();
+		for(int i=1; i<=a; i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita: "+skaitlis);
-		
+		}
     }
 	public static void main(String[] args) {
 		
-		mestkaulinu();
+		mestkaulinu(3);
 		
 	}
 
