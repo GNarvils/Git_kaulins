60acc1e0f471f0a774726b203b66dff622f639a0
Narvils <gustavs.gn@gmail.com>
2022-01-18 11:08:36 +0200

    Iespejams ievadit, cik reizes mest kaulinu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
 
@@ -6,9 +7,15 @@ public class Kaulins {
 		
 		int skaitlis;
 		Random rand = new Random();
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Cik reizes mest: ");
+		int reizes = scan.nextInt();
+
+		for(int i=1; i<=reizes; i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita: "+skaitlis);
-		
+		}
+		scan.close();
 	}
 
 }
