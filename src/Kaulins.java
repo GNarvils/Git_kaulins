ae70373b55083fa6442a5534bde441f09d48b851
Narvils <gustavs.gn@gmail.com>
2022-02-01 10:45:31 +0200

    Lielako skaitli 2

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 
 public class Kaulins {
     static void mestkaulinu(int a){
-		int skaitlis;
+		int skaitlis, lielakais = 0;
 		Random rand = new Random();
 		for(int i=1; i<=a; i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita: "+skaitlis);
+		if(lielakais<skaitlis){
+			lielakais = skaitlis;
 		}
+		}
+		System.out.println("Lielakais skaits metienu reize ir "+lielakais);
     }
 	public static void main(String[] args) {
 		Scanner scan =new Scanner(System.in);
